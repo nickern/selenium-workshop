000911c0b549cc3783d87272101c3935d55a907a
Nickern <nickern@gmail.com>
2018-05-14 23:31:19 +0200

    first browserstack commit - Not working yet!

---
+++ src/test/java/net/naglestad/examples/browserstack/SingleTest.java
@@ -0,0 +1,21 @@
+package net.naglestad.examples.browserstack;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+
+public class SingleTest extends BrowserStackJUnitTest {
+
+  @Test
+  public void test() throws Exception {
+    driver.get("https://www.google.com/ncr");
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("BrowserStack");
+    element.submit();
+    Thread.sleep(5000);
+
+    assertEquals("BrowserStack - Google Search", driver.getTitle());
+  }
+}
